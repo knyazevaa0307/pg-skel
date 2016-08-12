cfbfcb74dcce853e0de3f608bf105d2ab5ab4c64
Alexey Kovrizhkin <jean@tender.pro>
2016-08-12 10:59:41 +0300

    Init

---
+++ init.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Копирование файлов в системный каталог postgresql и создание template database
+#
+
+# strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+log() {
+  local d=$(date "+%F %T")
+  echo  "$d $1"
+}
+
+DB=$1
+
+SRC=/var/log/supervisor/pg_system
+D=/usr/share/postgresql/$PG_MAJOR/tsearch_data
+
+log "Setup postgresql system files as user $(id -un)"
+
+log "Copy translit"
+F=translit.rules
+[ -e $D/$F ] || cp $SRC/$F $D/
+
+log "Copy fts"
+pushd $SRC/fts-pg/tsearch_data > /dev/null
+for f in *.* ; do
+  [ -e $D/$f ] || cp $f $D/
+done
+popd  > /dev/null
+
+if gosu postgres psql -lqt | cut -d \| -f 1 | grep -qw $DB; then
+  log "Database '$DB' already exists, exiting"
+  exit 0
+fi
+
+log "Creating $DB..."
+gosu postgres createdb $DB
+
+log "Updating $DB extensions..."
+gosu postgres psql -d $DB -f $SRC/setup.sql
+
+log "Updating $DB FTS..."
+gosu postgres psql -d $DB -f $SRC/fts-pg/setup.sql
+
+log "Done"
---
+++ setup.sql
@@ -0,0 +1,12 @@
+-- ----------------------------------------------------------------------------
+-- setup iac database
+CREATE EXTENSION IF NOT EXISTS unaccent;
+ALTER TEXT SEARCH DICTIONARY unaccent (RULES='translit');
+CREATE EXTENSION IF NOT EXISTS ltree;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pg_buffercache;
+
+CREATE OR REPLACE LANGUAGE plpgsql;
+CREATE OR REPLACE LANGUAGE plperl;
